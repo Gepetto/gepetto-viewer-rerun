46e1849661816896d4c12b0aaf754a42716cd591
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-20 16:28:26 +0100

    pre-commit run -a

--- examples/basic_geometries.py
+++ examples/basic_geometries.py
@@ -13,19 +13,25 @@ def basic_geometries():
     rec.gui.addArrow("arrow", 0.5, -5, [100, 0, 50, 255])
     rec.gui.addToGroup("arrow", "s1")
 
-    rec.gui.addCapsule("capsule", 0.5, 5, [100, 0,155, 255])
+    rec.gui.addCapsule("capsule", 0.5, 5, [100, 0, 155, 255])
     rec.gui.addToGroup("capsule", "s1")
 
-    rec.gui.addLine("s1/line", (-5, -2, -4), (9, 4, -5), [100, 0,155, 255])
+    rec.gui.addLine("s1/line", (-5, -2, -4), (9, 4, -5), [100, 0, 155, 255])
 
-    rec.gui.addSquareFace("square", (-4, 0, 1), (2, 6, 2), (3, 2, 3), (5, 4, 4), [25, 50, 130, 255])
-    rec.gui.addTriangleFace("triangle", (5, 3, 1), (8, 6, -2), (5, 2, 3), [70, 30, 130, 255])
+    rec.gui.addSquareFace(
+        "square", (-4, 0, 1), (2, 6, 2), (3, 2, 3), (5, 4, 4), [25, 50, 130, 255]
+    )
+    rec.gui.addTriangleFace(
+        "triangle", (5, 3, 1), (8, 6, -2), (5, 2, 3), [70, 30, 130, 255]
+    )
     rec.gui.addToGroup("square", "s1")
     rec.gui.addToGroup("triangle", "s1")
 
     rec.gui.addSphere("s1/sphere", 2, [62, 255, 20, 255])
 
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='basic_geometries.log', filemode='w', level=logging.DEBUG)
+if __name__ == "__main__":
+    logging.basicConfig(
+        filename="basic_geometries.log", filemode="w", level=logging.DEBUG
+    )
     basic_geometries()
