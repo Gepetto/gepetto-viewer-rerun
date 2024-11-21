860877ec324ec48466a7a41825fbb94028fc418e
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-11-21 12:16:06 +0100

    nix packaging

---
+++ default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  setuptools,
+  rerun,
+}:
+
+buildPythonPackage {
+  pname = "gepetto-viewer-rerun";
+  version = "0.1.0-unstable-2024-11-20";
+  pyproject = true;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./examples
+      ./pyproject.toml
+      ./README.md
+      ./src
+      ./uv.lock
+    ];
+  };
+
+  build-system = [
+    setuptools
+  ];
+
+  dependencies = [
+    rerun
+  ];
+
+  pythonImportsCheck = [
+    "gepetto_viewer_rerun"
+  ];
+
+  meta = {
+    description = "Provide a `Client` class to visualize data with Rerun";
+    homepage = "https://github.com/gepetto/gepetto-viewer-rerun";
+    license = lib.licenses.bsd2;
+    maintainers = with lib.maintainers; [ nim65s ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Provide a `Client` class to visualize data with Rerun";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          apps.default = {
+            type = "app";
+            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+          };
+          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          packages = {
+            default = self'.packages.gepetto-viewer-rerun;
+            gepetto-viewer-rerun = pkgs.python3Packages.callPackage ./. { };
+          };
+        };
+    };
+}
