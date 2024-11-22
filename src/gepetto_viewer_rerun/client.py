999c37d441a13ae2b9ff4891323d9a534a556bcd
EtaLoop <146718995+EtaLoop@users.noreply.github.com>
2024-11-22 15:58:29 +0100

    Fix getWindowList()

--- src/gepetto_viewer_rerun/client.py
+++ src/gepetto_viewer_rerun/client.py
@@ -75,7 +75,7 @@ class Gui:
         logger.info(msg)
 
     def getWindowList(self):
-        return self.windowList
+        return [scene.name for scene in self.windowList]
 
     def getSceneList(self):
         return [scene.name for scene in self.sceneList]
