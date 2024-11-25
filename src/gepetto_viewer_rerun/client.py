536360138ff157362733a4bd74ca9e9dc3df3c69
EtaLoop <146718995+EtaLoop@users.noreply.github.com>
2024-11-25 14:18:21 +0100

    Refactor getNodeList()
    Co-authored-by: Guilhem Saurel <guilhem.saurel@laas.fr>

--- src/gepetto_viewer_rerun/client.py
+++ src/gepetto_viewer_rerun/client.py
@@ -90,10 +90,7 @@ class Gui:
         return name
 
     def getNodeList(self):
-        entitiesName = []
-        for entity_type in self.entityList:
-            for entity in entity_type:
-                entitiesName.append(entity.name)
+        entitiesName = [entity.name for entity_type in self.entityList for entity in entity_type]
         return self.getWindowList() + self.getSceneList() + entitiesName
 
     def nodeExists(self, nodeName: str):
