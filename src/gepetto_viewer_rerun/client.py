cb489bdabd66ba7aea7bcafd53eb50d7a64027e9
EtaLoop <146718995+EtaLoop@users.noreply.github.com>
2024-12-04 17:06:35 +0100

    Add addToGroup & createGroup

--- src/gepetto_viewer_rerun/client.py
+++ src/gepetto_viewer_rerun/client.py
@@ -6,7 +6,7 @@ from typing import List, Union
 import numpy as np
 import rerun as rr
 
-from .entity import Entity, MeshFromPath
+from .entity import Entity, MeshFromPath, Group
 from .scene import Scene
 
 logger = logging.getLogger(__name__)
@@ -43,12 +43,14 @@ class Gui:
         self.scene_list = []
         self.window_list = []
         self.entity_list = [[] for _ in range(len(Archetype))]
+        self.group_list = []
 
     def __repr__(self):
         return (
-            f"Gui(window_list={self.window_list}, "
-            f"scene_list (size: {len(self.scene_list)}) = {self.scene_list}, "
-            f"entity_list (size: {len(self.entity_list)}) = {self.entity_list})"
+            f"Gui(window_list={self.window_list}\n"
+            f"scene_list (size: {len(self.scene_list)}) = {self.scene_list}\n"
+            f"entity_list (size: {len(self.entity_list)}) = {self.entity_list})\n"
+            f"group_list (size: {len(self.group_list)}) = {self.group_list})"
         )
 
     def createWindow(self, name: str) -> str:
@@ -560,18 +562,163 @@ class Gui:
         )
         return True
 
+    def _log_entity(self, entity: Entity):
+        """Draw a group entity in the Viewer."""
+        if entity.scenes is not None:  # TODO: Remove None by default
+            for scene in entity.scenes:
+                rr.log(
+                    entity.log_name,
+                    entity.archetype,
+                    recording=scene.rec,
+                )
+                logger.info(
+                    f"_log_entity(): Logging entity '{entity.name}' in '{scene.name}' scene."
+                )
+            return True
+        logger.info(
+            f"_log_entity(): Logging entity '{entity.name}' don't have any scenes to be displayed in."
+        )
+        return False
+
+    def _get_group_list(self, group_name: str) -> List[Group] | None:
+        """Get group inside `self.group_List`"""
+        group_list = []
+        for group in self.group_list:
+            if group.name.strip("/").endswith(group_name):
+                group_list.append(group)
+        return group_list
+
+    def _format_string(self, first: str, second: str) -> str:
+        """Add '/' between `first` and `second`."""
+        return first.strip("/") + "/" + second.strip("/")
+
+    def _get_added_group(self, groupName: str) -> List[Group]:
+        """
+        Get all the nodes named 'groupName',
+            return the list of nodes that are childrens of other nodes.
+        Eg: in this list ["test", "world/test", "map/test"]
+        _choose_group will return ["world/test", "map/test"]
+        """
+
+        g_list = self._get_group_list(groupName)
+        new_g_list = []
+        if len(g_list) == 1:
+            return [g_list[0]]
+        for group in g_list:
+            if "/" in group.name.strip("/"):
+                new_g_list.append(group)
+        return new_g_list
+
+    def _get_children_entity_in_group(self, group_name: str) -> List[Entity]:
+        """Return all the children entity of a group"""
+
+        children = []
+        for archetype in self.entity_list:
+            for entity in archetype:
+                if group_name in entity.log_name:
+                    children.append(entity)
+        return children
+
+    def _add_entity_to_scene(self, entity: Entity, scene_index: int):
+        """Add Entity to Scene"""
+        scene = self.scene_list[scene_index]
+        entity.addScene(scene)
+        entity.set_log_name(entity.name)
+        logger.info(
+            f"addToGroup(): Add entity '{entity.name}' to '{scene.name}' scene."
+        )
+        self._log_entity(entity)
+
+    def _add_entity_to_group(
+        self, entity: Entity, group_name_list: List[Group], groupName: str
+    ):
+        """Add Entity to Group"""
+        group_name_list = self._get_added_group(groupName)
+        for group in group_name_list:
+            for scene in group.scenes:
+                entity.addScene(scene)
+
+            entity.set_log_name(self._format_string(group.name, entity.name))
+            self._log_entity(entity)
+        logger.info(f"addToGroup(): Add entity '{entity.name}' to '{groupName}' group.")
+
+    def _add_group_to_scene(
+        self, node_name_list: List[Group], scene_index: int, group_name: str
+    ):
+        """Add Group to a Scene"""
+        scene = self.scene_list[scene_index]
+        for group in node_name_list:
+            group.add_scene(scene)
+
+            # Add scene for all children of the group
+            children = self._get_children_entity_in_group(group_name)
+            for child in children:
+                child.addScene(self.scene_list[scene_index])
+                self._log_entity(child)
+        logger.info(f"addToGroup(): Add group '{group_name}' to '{scene.name}' scene.")
+
+    def _add_group_to_group(
+        self,
+        group_name_list: List[Group],
+        node_name_list: List[Group],
+        group1_name: str,
+        group2_name: str,
+    ):
+        """Add Group to Group"""
+        new_group = Group(self._format_string(group2_name, group1_name))
+        for group in group_name_list:
+            for scene in group.scenes:
+                new_group.add_scene(scene)
+                # Ensure that the added group 'nodeName' has its `scenes` filled
+                for group1 in node_name_list:
+                    group1.add_scene(scene)
+        self.group_list.append(new_group)
+        logger.info(
+            f"addToGroup(): Add group '{group1_name}' to '{group2_name}' group."
+        )
+
     def addToGroup(self, nodeName: str, groupName: str) -> bool:
         """
         Actual log of an entity
+        add group1 to a group2 will create another group 'group1/group2'
         """
         assert all(
             isinstance(name, str) for name in [nodeName, groupName]
         ), "Parameters 'nodeName' and 'groupName' must be strings"
 
-        if self._get_scene_index(groupName) == -1:
-            logger.error(f"addToGroup(): Scene '{groupName}' does not exists.")
+        entity = self._get_entity(nodeName)
+        node_name_list = self._get_group_list(nodeName)
+        if entity is None and not node_name_list:
+            logger.error(f"addToGroup(): Node '{nodeName}' does not exists.")
             return False
-        if not self._get_entity(nodeName):
-            logger.error(f"addToGroup(): Entity '{nodeName}' does not exists.")
+
+        scene_index = self._get_scene_index(groupName)
+        group_name_list = self._get_group_list(groupName)
+        if not group_name_list and scene_index == -1:
+            logger.error(f"addToGroup(): Group '{groupName}' does not exists.")
             return False
-        return self._log_archetype(nodeName, groupName)
+
+        if entity:
+            if scene_index != -1:
+                self._add_entity_to_scene(entity, scene_index)
+            elif group_name_list:
+                self._add_entity_to_group(entity, group_name_list, groupName)
+            else:
+                return False
+        elif node_name_list:
+            if scene_index != -1:
+                self._add_group_to_scene(node_name_list, scene_index, nodeName)
+            elif group_name_list:
+                self._add_group_to_group(
+                    group_name_list, node_name_list, nodeName, groupName
+                )
+            else:
+                return False
+        return True
+
+    def createGroup(self, groupName: str) -> bool:
+        assert isinstance(groupName, str), "Paramter 'groupName' must be a string"
+
+        self.group_list.append(Group(groupName))
+        logger.info(f"createGroup(): create group '{groupName}'.")
+        return True
--- src/gepetto_viewer_rerun/entity.py
+++ src/gepetto_viewer_rerun/entity.py
@@ -1,5 +1,5 @@
 import rerun as rr
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Union, List
 import pathlib
 from .scene import Scene
@@ -14,6 +14,7 @@ class Entity:
 
     name: str
     archetype: rr.archetypes
+    log_name: str = ""
     scenes: List[Scene] | None = None
 
     def __post_init__(self):
@@ -27,9 +28,29 @@ class Entity:
             scene, Scene
         ), "Entity.addScene() parameter 'scene' must be of type 'Scene'"
 
-        self.scenes.append(scene)
+        if scene not in self.scenes:
+            self.scenes.append(scene)
+
+    def set_log_name(self, name: str):
+        """Set log_name"""
+        self.log_name = name
 
 
 @dataclass
 class MeshFromPath:
     path: Union[str, pathlib.Path]
+
+
+@dataclass
+class Group:
+    name: str
+    scenes: List[Scene] = field(default_factory=list)
+
+    def add_scene(self, scene: Scene):
+        """Add `scene` to self.scenes."""
+        assert isinstance(
+            scene, Scene
+        ), "Group.add_scene(): Parameter 'scene' must be a `Scene`"
+
+        if scene not in self.scenes:
+            self.scenes.append(scene)
